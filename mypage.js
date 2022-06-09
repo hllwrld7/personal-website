43e0f8514cc8e0d95a24bb3cc091b953e6bf097a
hllwrld7 <101736550+hllwrld7@users.noreply.github.com>
2022-06-09 12:58:03 +0200

    A very raw version of a website
    I'm aware it looks really really bad, still figuring out how to do the design.

---
+++ mypage.js
@@ -0,0 +1,74 @@
+function menuColour()
+{
+	document.getElementsByClassName("menuButton").style.backgroundColor = "rgb(127, 149, 209)";
+}
+
+// function changeColourOver(id)
+// {
+// 	interval = setInterval(frame, 5);
+// 	let r = 127;
+// 	let g = 149;
+// 	let b = 209;
+// 	function frame()
+// 	{
+// 		if(r == 255 && g = 130 && b == 169)
+// 			{
+// 				clearInterval(interval);
+// 			}
+// 		if(r == 255)
+// 			{
+// 				r = 255;
+// 			}
+// 		if(g == 130)
+// 			{
+// 				g = 130;
+// 			}
+// 		if(b == 169)
+// 			{
+// 				b = 169;
+// 			}
+// 		else
+// 			{
+// 				r++;
+// 				g--;
+// 				b--;
+// 				document.getElementById(id).style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+// 			}
+// 	}
+// //	document.getElementById(id).style.backgroundColor = "rgb(255, 130, 169)";
+// }
+
+// function changeColourOut(id)
+// {
+// 	interval = setInterval(frame, 5);
+// 	let r = 255;
+// 	let g = 130;
+// 	let b = 169;
+// 	function frame()
+// 	{
+// 		if(r == 127 && g = 149 && b == 209)
+// 			{
+// 				clearInterval(interval);
+// 			}
+// 		if(r == 127)
+// 			{
+// 				r = 127;
+// 			}
+// 		if(g == 149)
+// 			{
+// 				g = 149;
+// 			}
+// 		if(b == 209)
+// 			{
+// 				b = 209;
+// 			}
+// 		else
+// 			{
+// 				r--;
+// 				g++;
+// 				b++
+// 				document.getElementById(id).style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+// 			}
+// 	}
+// //	document.getElementById(id).style.backgroundColor = "rgb(127, 149, 209)";
+// }
